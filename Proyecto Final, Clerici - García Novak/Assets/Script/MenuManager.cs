a10eca398a22a1fa8c125fd114b5d292f7d961cd
mariangnovak <marianonovak@hotmail.com>
2022-07-23 21:26:23 -0300

    Basic Tileboard

---
+++ Proyecto Final, Clerici - García Novak/Assets/Script/MenuManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuManager
+{
+    [MenuItem("Component/Comiste")]
+
+    public static void Comiste()
+    {
+
+    }
+}
